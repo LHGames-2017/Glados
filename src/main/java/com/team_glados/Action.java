6e364ce3f807a29339dd5e4a6946ff1b4f5a94e0
Unknown <simonlary@gmail.com>
2017-10-07 12:03:51 -0400

    Setup default action classes

---
+++ src/main/java/com/team_glados/Action.java
@@ -0,0 +1,7 @@
+package com.team_glados;
+
+import com.jeremycurny.sparkjavarestapi.util.GameInfo;
+
+public interface Action {
+	public int getWeight(GameInfo info);
+}
---
+++ src/main/java/com/team_glados/SuicideAction.java
@@ -0,0 +1,9 @@
+package com.team_glados;
+
+import com.jeremycurny.sparkjavarestapi.util.GameInfo;
+
+public class SuicideAction implements Action {
+	public int getWeight(GameInfo info) {
+		return 0;
+	}
+}
