3f12d3f32af89aa1baf4e73a63b78394fbf135b2
Unknown <simonlary@gmail.com>
2017-10-07 13:30:12 -0400

    Create functionnal HealAction

--- src/main/java/com/team_glados/actions/ShopAction.java
+++ src/main/java/com/team_glados/actions/ShopAction.java
@@ -3,6 +3,13 @@ package com.team_glados.actions;
 import com.jeremycurny.sparkjavarestapi.util.GameInfo;
 
 public class ShopAction extends AbstractAction {
+
+	static boolean hasSword = false;
+	static boolean hasShield = false;
+	static boolean hasBackpack = false;
+	static boolean hasPickaxe = false;
+	static boolean hasHealthPotion = false;
+
 	@Override
 	public int getWeight(GameInfo info) {
 		return 0;
