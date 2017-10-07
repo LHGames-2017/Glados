a274d7f0b97986403f55a11d1a31b598fd0bf4e4
Unknown <simonlary@gmail.com>
2017-10-07 12:40:09 -0400

    Working UserController

--- src/main/java/com/team_glados/UserController.java
+++ src/main/java/com/team_glados/UserController.java
@@ -3,6 +3,7 @@ package com.team_glados;
 import com.jeremycurny.sparkjavarestapi.controller.RestController;
 import com.jeremycurny.sparkjavarestapi.util.AiHelper;
 import com.jeremycurny.sparkjavarestapi.util.GameInfo;
+import com.team_glados.actions.*;
 import spark.Request;
 import spark.Response;
 
@@ -10,15 +11,45 @@ import java.net.URLDecoder;
 
 public class UserController extends RestController {
 
+	private Action[] actions;
+
+	public UserController() {
+		actions = new Action[]{
+				new AttackAction(),
+				new CollectAction(),
+				new HealAction(),
+				new MoveToCollectAction(),
+				new MoveToHouseAction(),
+				new MoveToOtherHouseAction(),
+				new MoveToPlayerAction(),
+				new MoveToShopAction(),
+				new RunAwayFromPlayerAction(),
+				new ShopAction(),
+				new StealAction(),
+				new SuicideAction(),
+				new UpgradeAction()
+		};
+	}
+
 	@Override
 	public Object bot(Request req, Response res) {
 		String s = URLDecoder.decode(req.body()).substring(4);
 		GameInfo gameInfo = new GameInfo();
 		gameInfo.fromJson(s);
 
-		// AI IMPLEMENTATION HERE.
+		int[] weights = new int[actions.length];
+		int highestIndex = 0;
+		int highestValue = 1000;
+		for (int i = 0; i < actions.length; i++) {
+			weights[i] = actions[i].getWeight(gameInfo);
+			if (weights[i] > highestValue) {
+				highestIndex = i;
+				highestValue = weights[i];
+			}
+		}
 
-		String action = AiHelper.CreateMoveAction(gameInfo.player.Position);
-	    return super.resJson(req, res, 200, action);
+		System.out.print(actions[highestIndex].getClass().getName());
+		String action = actions[highestIndex].doIt(gameInfo);
+		return super.resJson(req, res, 200, action);
 	}
 }
