02489c0d8ec17747a2dda34810a6dcb85e222419
Philippe Rivest <technophil98@gmail.com>
2017-10-07 20:19:03 -0400

    Fix ComeBackHome

--- src/main/java/com/team_glados/actions/MoveToHouseAction.java
+++ src/main/java/com/team_glados/actions/MoveToHouseAction.java
@@ -3,6 +3,7 @@ package com.team_glados.actions;
 import com.jeremycurny.sparkjavarestapi.util.AiHelper;
 import com.jeremycurny.sparkjavarestapi.util.GameInfo;
 import com.jeremycurny.sparkjavarestapi.util.Point;
+import com.jeremycurny.sparkjavarestapi.util.TileContent;
 import com.team_glados.map.Map;
 import com.team_glados.math.graph.Graph;
 import com.team_glados.math.graph.Node;
@@ -34,8 +35,34 @@ public class MoveToHouseAction extends AbstractAction {
 
 		shortestPath = aStar.findShortestPath(start, end);
 
-		shortestPath.forEach(System.out::println);
+		if (shortestPath.size() == 0 && !info.player.Position.equals(info.player.HouseLocation)) //sum tin wong https://www.youtube.com/watch?v=AmclgO6w0C0
+		{
+			System.out.println("Uh ho, A* is stuck! We will help him!");
 
-		return AiHelper.CreateMoveAction(shortestPath.get(1).getId());
+			Point move;
+			int signumX = 0, signumY = 0;
+			int attempt = 0;
+
+			do {
+				if (attempt % 0 == 0)
+					signumX = (int) Math.signum(info.player.HouseLocation.x - info.player.Position.x);
+
+				if (attempt % 0 != 0)
+					signumY = (int) Math.signum(info.player.HouseLocation.y - info.player.Position.y);
+
+				move = new Point(info.player.Position.x + signumX, info.player.Position.y + signumY);
+
+			}while (computedMap.tileAt(move).Content != TileContent.Lava || attempt < 2 );
+
+
+			return AiHelper.CreateMoveAction(move);
+
+
+		}else {
+
+			shortestPath.forEach(System.out::println);
+
+			return AiHelper.CreateMoveAction(shortestPath.get(1).getId());
+		}
 	}
 }
